5434eb78381f09b30a59c96b973865907b56183d
François Pluchino <francois.pluchino@sonatra.com>
2015-09-19 21:08:11 +0200

    Add pjax lifecycle for select2 scroller component

---
+++ js/components/sonatra-select2-scroller.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+
+/**
+ * @param {jQuery} $
+ *
+ * @typedef {object} define.amd
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'jquery-pjax', 'app-pjax', 'sonatra-select2-scroller'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // APP PJAX COMPONENT REGISTER DEFINITION
+    // ======================================
+
+    $.fn.appPjax.Constructor.API_REGISTERS.push(function (appPjax) {
+        $('[data-select2-jquery-scroller="true"]', appPjax.$container).each(function () {
+            var $this = $(this);
+            $.fn.select2Scroller.call($this, $this.data());
+        });
+    });
+}));
