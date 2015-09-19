00dc656656032cad8e9b5d84bc1080ece7ba0ccd
François Pluchino <francois.pluchino@sonatra.com>
2015-09-19 20:57:11 +0200

    Add pjax lifecycle for bootstrap filestyle component

---
+++ js/components/bootstrap-filestyle.js
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+
+/**
+ * @param {jQuery} $
+ *
+ * @typedef {object} define.amd
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'jquery-pjax', 'app-pjax', 'bootstrap-filestyle'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // APP PJAX COMPONENT REGISTER DEFINITION
+    // ======================================
+
+    $.fn.appPjax.Constructor.API_REGISTERS.push(function (appPjax) {
+        $('.filestyle', appPjax.$container).each(function () {
+            var $this = $(this),
+                options = {
+                    'input' : $this.attr('data-input') !== 'false',
+                    'icon' : $this.attr('data-icon') !== 'false',
+                    'buttonBefore' : $this.attr('data-buttonBefore') === 'true',
+                    'disabled' : $this.attr('data-disabled') === 'true',
+                    'size' : $this.attr('data-size'),
+                    'buttonText' : $this.attr('data-buttonText'),
+                    'buttonName' : $this.attr('data-buttonName'),
+                    'iconName' : $this.attr('data-iconName'),
+                    'badge' : $this.attr('data-badge') !== 'false',
+                    'placeholder': $this.attr('data-placeholder')
+                };
+
+            $this.filestyle(options);
+        });
+    });
+}));
