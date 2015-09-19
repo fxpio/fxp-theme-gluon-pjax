3bfb942c00fef9c194db1cdef6ac55b033fb82fb
François Pluchino <francois.pluchino@sonatra.com>
2015-09-19 21:05:15 +0200

    Add pjax lifecycle for sidebar component

---
+++ js/components/sonatra-jquery-sidebar.js
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+
+/**
+ * @param {jQuery} $
+ *
+ * @typedef {object} define.amd
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'jquery-pjax', 'app-pjax', 'sonatra-jquery-sidebar', 'sonatra-theme-gluon/js/sidebar-context'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // APP PJAX COMPONENT REGISTER DEFINITION
+    // ======================================
+
+    $.fn.appPjax.Constructor.API_REGISTERS.push(function (appPjax) {
+        $('[data-sidebar="true"]', appPjax.$container).each(function () {
+            var $this = $(this);
+            $.fn.sidebar.call($this, $this.data());
+        });
+    });
+
+    // APP PJAX COMPONENT DESTROYER DEFINITION
+    // =======================================
+
+    $.fn.appPjax.Constructor.API_DESTROYERS.push(function (appPjax) {
+        $('[data-sidebar="true"]', appPjax.$container).each(function () {
+            var $this = $(this);
+            $.fn.sidebar.call($this, 'destroy');
+        });
+    });
+}));
