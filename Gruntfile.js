07a0f8615aefeb3ee2d474b38b19e9be69600038
François Pluchino <francois.pluchino@sonatra.com>
2015-09-19 20:26:50 +0200

    Add gruntfile config

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global module*/
+/*global require*/
+/*global grunt*/
+/*global setTimeout*/
+
+module.exports = function (grunt) {
+    'use strict';
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        less: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    "css/app-pjax.css": "less/app-pjax.less"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', [
+        'less'
+    ]);
+};
