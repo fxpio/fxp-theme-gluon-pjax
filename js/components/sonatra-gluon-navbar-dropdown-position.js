ea14534a824ea26abcb4c9049064cffa38a818d7
François Pluchino <francois.pluchino@sonatra.com>
2016-06-27 11:51:25 +0200

    Add navbar dropdown position pjax component

---
+++ js/components/sonatra-gluon-navbar-dropdown-position.js
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*global define*/
+/*global jQuery*/
+
+/**
+ * @param {jQuery} $
+ *
+ * @typedef {object} define.amd
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+(function (factory) {
+    'use strict';
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'jquery-pjax', 'app-pjax', 'bootstrap/js/dropdown', 'sonatra-theme-gluon/js/navbar-dropdown-position'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+    'use strict';
+
+    // APP PJAX COMPONENT REGISTER DEFINITION
+    // ======================================
+
+    $.fn.appPjax.Constructor.API_REGISTERS.push(function ($container) {
+        $('.navbar-fixed-top, .navbar-fixed-bottom', $container).each(function () {
+            var $this = $(this);
+            $.fn.navbarDropdownPosition.call($this, $this.data());
+        });
+    });
+
+    // APP PJAX COMPONENT DESTROYER DEFINITION
+    // =======================================
+
+    $.fn.appPjax.Constructor.API_DESTROYERS.push(function ($container) {
+        $('.navbar-fixed-top, .navbar-fixed-bottom', $container).each(function () {
+            var $this = $(this);
+            $.fn.navbarDropdownPosition.call($this, 'destroy');
+        });
+    });
+}));
